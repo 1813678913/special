f514024ac9bcf8098256547c62fe6f1611816f03
zhangwanfu <zhangwanfu@szmsd.com>
2021-01-11 22:52:59 +0800

    1.增加语句

--- src/main/java/com/wf/controller/UserController.java
+++ src/main/java/com/wf/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class UserController {
@@ -49,8 +50,8 @@ public class UserController {
         return new ResultJson<>(1);
     }
 
-    @RequestMapping("/home")
-    public ResultJson<List<String>> home() {
+    @GetMapping("/home")
+    public ResultJson<String> home() {
         list.add("盛年不重来，一日难再晨。及时宜自勉，岁月不待人。——陶渊明");
         list.add("君子之交淡若水，小人之交甘若醴，君子淡以亲，小人甘以绝。——庄子");
         list.add("三更灯火五更鸡，正是男儿读书时，黑发不知勤学早，白首方悔读书迟。——颜真卿");
@@ -63,15 +64,17 @@ public class UserController {
         list.add("人不可有傲气，但不可无傲骨。——徐悲鸿");
         list.add("静以修身，俭以养德，非淡泊无以明志，非宁静无以致远。——诸葛亮");
         list.add("天时不如地利，地利不如人和。——《孟子·公孙丑》");
-        list.add("三人行，必有我师焉，择其善者而从之，其不善者而改之。——孔子");
+        list.add("追求源于热爱");
         list.add("少而好学，如日出之阳；壮而好学，如日中之光；志而好学，如炳烛之光。——刘向");
         list.add("古之立大志者，不惟有超世之才，亦必有坚韧不拔之志。——苏轼");
         list.add("盛年不重来，一日难再晨。及时当勉励，岁月不待人。——陶渊明");
         list.add("三军可夺帅也，匹夫不可夺志也。——孔子");
         list.add("天下事以难而废者十之一，以惰而废者十之九。——颜之推");
         list.add("时间就是生命，无端地空耗别人的时间，无异于谋财害命。——鲁迅");
-        list.add("尺有所短；寸有所长。物有所不足；智有所不明。——屈原《卜居》");
-        return new ResultJson<>(list);
+        list.add("永远相信美好的事情即将发生");
+        list.add("须知少年凌云志，曾许人间第一流");
+        int i = new Random().nextInt(22);
+        return new ResultJson<>(list.get(i));
     }
 
 
