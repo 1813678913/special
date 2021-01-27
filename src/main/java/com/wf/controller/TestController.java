c36274d91f99a4ba74bcbd497eb400ea92becf9f
1813678913 <1813678913@qq.com>
2021-01-27 17:47:48 +0800

    1.新增下载

--- src/main/java/com/wf/controller/TestController.java
+++ src/main/java/com/wf/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wf.controller;
 
 
+import com.wf.utils.ResultJson;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,28 +13,31 @@ import java.net.URLConnection;
 @RestController
 public class TestController {
 
+    private static int count=0;
+
     @RequestMapping("/download")
-    public String allUser(String urls, String path) {
-        if (null == urls || null == path) {
-            return "下载路径或者下载链接不能为空";
+    public ResultJson<String> allUser(String urls, String paths) {
+        if (null == urls || null == paths) {
+            return new ResultJson<>("下载路径或者下载链接不能为空");
         }
-        path = path + "\\download.pdf";
+        paths = paths + "\\download"+count+".pdf";
         try {
             URL url = new URL(urls);
             URLConnection urlConnection = url.openConnection();
             InputStream inputStream = urlConnection.getInputStream();
             byte[] buff = new byte[10240];
             int temp;
-            FileOutputStream outputStream = new FileOutputStream(path);
+            FileOutputStream outputStream = new FileOutputStream(paths);
             while ((temp = inputStream.read(buff)) != -1) {
                 outputStream.write(buff, 0, temp);
             }
+            count++;
             outputStream.close();
             inputStream.close();
         } catch (Exception e) {
-            return "程序出错，可能方法失效，或者操作有误！错误信息" + e.getMessage();
+            return new ResultJson<>("程序出错，可能方法失效，或者操作有误！错误信息" + e.getMessage());
         }
-        return "下载成功";
+        return new ResultJson<>("下载成功");
     }
 
 }
